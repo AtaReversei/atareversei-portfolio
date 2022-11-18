be5d0d2b411f425ea7e13583a23b65808a56485b
atareversei <faraji.ata92@gmail.com>
2022-11-18 15:41:30 +0330

    [ADD] skills section and overall structure

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import './home.scss'
 import Hero from "../../macro-components/Hero/Hero";
+import Skills from "../../macro-components/Skills/Skills";
 
 const Home = () => {
     return (
         <>
             <Hero />
+            <Skills />
         </>
     )
 }
