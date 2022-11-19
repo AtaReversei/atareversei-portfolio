8f744d5f5a84b2e18281176a73fc51bac7a4dd92
atareversei <faraji.ata92@gmail.com>
2022-11-19 23:32:08 +0330

    [ADD] structure to about me

--- client/src/macro-components/Skills/Skills.js
+++ client/src/macro-components/Skills/Skills.js
@@ -4,14 +4,14 @@ import ScrollContainer from 'react-indiana-drag-scroll'
 const Skills = () => {
 
     return (
-        <section className="container padding-top-section">
+        <section className="container padding-top-large">
             <h2 className="gradient-text">
                 About Me
             </h2>
             <p>
                 Drag and scroll to see more!
             </p>
-            <div className="skills__grid-container padding-top-medium">
+            <div className="skills__grid-container">
                 <ScrollContainer className="skills__grid" draggingClassName="skills__grid-on-drag">
                     <div className="skill__box skill__el-1">1</div>
                     <div className="skill__box skill__el-2">2</div>
@@ -25,12 +25,19 @@ const Skills = () => {
                     <h3 className="skill__title skill__el-10">A React Developer.</h3>
                     <p className="skill__text skill__el-11">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur provident, voluptatibus. Aliquam beatae consequatur sapiente!</p>
                     <div className="skill__box skill__el-12">12</div>
-                    <div className="skill__design-box-3 skill__el-13"></div>
+                    <h3 className="skill__design-box-3 skill__el-13">*</h3>
                     <div className="skill__box skill__el-14">14</div>
                     <div className="skill__box skill__el-15">15</div>
                     <div className="skill__box skill__el-16">16</div>
                     <div className="skill__box skill__el-17">17</div>
-
+                    <h3 className="skill__design-box-4 skill__el-18">
+                        <i className="fa-solid fa-arrow-down-right"></i>
+                    </h3>
+                    <h3 className="skill__title gradient-text skill__el-19">Commit Everyday,<br/>Live a Happy Life</h3>
+                    <div className="skill__design-box-0 skill__el-20"></div>
+                    <div className="skill__box skill__el-21">21</div>
+                    <div className="skill__box skill__el-22">22</div>
+                    <div className="skill__box skill__el-23">23</div>
 
 
                 </ScrollContainer>
