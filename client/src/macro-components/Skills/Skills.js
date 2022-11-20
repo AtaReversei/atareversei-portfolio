946b836caa9e83dec7a40263b2fff64734ca835c
atareversei <faraji.ata92@gmail.com>
2022-11-20 12:16:15 +0330

    [ADD] structure to about me ii

--- client/src/macro-components/Skills/Skills.js
+++ client/src/macro-components/Skills/Skills.js
@@ -1,5 +1,6 @@
 import './skills.scss'
 import ScrollContainer from 'react-indiana-drag-scroll'
+import CommitHeatmap from "../../micro-components/Commit-Heatmap/CommitHeatmap";
 
 const Skills = () => {
 
@@ -15,7 +16,7 @@ const Skills = () => {
                 <ScrollContainer className="skills__grid" draggingClassName="skills__grid-on-drag">
                     <div className="skill__box skill__el-1">1</div>
                     <div className="skill__box skill__el-2">2</div>
-                    <p className="skill__text skill__el-3">I am somehow very long text.</p>
+                    <p className="skill__text skill__el-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque fuga ipsa ipsam labore nobis nulla sint temporibus totam, veniam voluptas!</p>
                     <div className="skill__box skill__el-4">4</div>
                     <div className="skill__box skill__el-5">5</div>
                     <div className="skill__design-box-1 skill__el-6">6</div>
@@ -38,6 +39,14 @@ const Skills = () => {
                     <div className="skill__box skill__el-21">21</div>
                     <div className="skill__box skill__el-22">22</div>
                     <div className="skill__box skill__el-23">23</div>
+                    <h3 className="skill__title skill__el-24">My React<br/>Toolkit</h3>
+                    <h3 className="skill__title gradient-text skill__el-25"><i className="fa-solid fa-heart"></i></h3>
+                    <div className="skill__box skill__el-26">26</div>
+                    <div className="skill__box skill__el-27">27</div>
+                    <div className="skill__box skill__el-28">28</div>
+                    <div className="skill__design-box-0 skill__el-29"><CommitHeatmap/></div>
+
+
 
 
                 </ScrollContainer>
---
+++ client/src/micro-components/Commit-Heatmap/CommitHeatmap.js
@@ -0,0 +1,74 @@
+import './commit-heatmap.scss'
+
+const CommitHeatmap = () => {
+    return (
+        <div className="commit-heatmap__container">
+            <div className="commit-heatmap__level-4"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-5"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-0"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-0"></div>
+            <div className="commit-heatmap__level-0"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-4"></div>
+            <div className="commit-heatmap__level-4"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-0"></div>
+            <div className="commit-heatmap__level-4"></div>
+            <div className="commit-heatmap__level-5"></div>
+            <div className="commit-heatmap__level-5"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-4"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-5"></div>
+            <div className="commit-heatmap__level-0"></div>
+            <div className="commit-heatmap__level-0"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-0"></div>
+            <div className="commit-heatmap__level-0"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-4"></div>
+            <div className="commit-heatmap__level-5"></div>
+            <div className="commit-heatmap__level-4"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-4"></div>
+            <div className="commit-heatmap__level-5"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-0"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-0"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-0"></div>
+            <div className="commit-heatmap__level-4"></div>
+            <div className="commit-heatmap__level-5"></div>
+            <div className="commit-heatmap__level-4"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-0"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-4"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-4"></div>
+            <div className="commit-heatmap__level-5"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-4"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-3"></div>
+            <div className="commit-heatmap__level-4"></div>
+        </div>
+    )
+}
+
+export default CommitHeatmap
